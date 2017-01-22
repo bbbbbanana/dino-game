dd7deb8b2182c5ccd4d46a0df3a325b4a359b4ee
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2017-01-22 14:59:28 +0800

    Add files via upload

---
+++ Cosmo Dino Game/Cosmo Dino Game/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Cosmo_Dino_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titlePic = new System.Windows.Forms.PictureBox();
+            this.instBtn = new System.Windows.Forms.PictureBox();
+            this.startGameBtn = new System.Windows.Forms.PictureBox();
+            this.dinoDiplo = new System.Windows.Forms.PictureBox();
+            this.dinoSteggy = new System.Windows.Forms.PictureBox();
+            this.dinoParalolo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.titlePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startGameBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dinoDiplo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dinoSteggy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dinoParalolo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titlePic
+            // 
+            this.titlePic.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titlePic.BackColor = System.Drawing.Color.Transparent;
+            this.titlePic.Image = ((System.Drawing.Image)(resources.GetObject("titlePic.Image")));
+            this.titlePic.Location = new System.Drawing.Point(524, 82);
+            this.titlePic.Margin = new System.Windows.Forms.Padding(0);
+            this.titlePic.Name = "titlePic";
+            this.titlePic.Size = new System.Drawing.Size(573, 109);
+            this.titlePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.titlePic.TabIndex = 0;
+            this.titlePic.TabStop = false;
+            // 
+            // instBtn
+            // 
+            this.instBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.instBtn.BackColor = System.Drawing.Color.Transparent;
+            this.instBtn.Image = ((System.Drawing.Image)(resources.GetObject("instBtn.Image")));
+            this.instBtn.Location = new System.Drawing.Point(624, 268);
+            this.instBtn.Name = "instBtn";
+            this.instBtn.Size = new System.Drawing.Size(372, 138);
+            this.instBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.instBtn.TabIndex = 1;
+            this.instBtn.TabStop = false;
+            this.instBtn.Click += new System.EventHandler(this.instBtnClick);
+            // 
+            // startGameBtn
+            // 
+            this.startGameBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startGameBtn.BackColor = System.Drawing.Color.Transparent;
+            this.startGameBtn.Image = ((System.Drawing.Image)(resources.GetObject("startGameBtn.Image")));
+            this.startGameBtn.Location = new System.Drawing.Point(624, 460);
+            this.startGameBtn.Name = "startGameBtn";
+            this.startGameBtn.Size = new System.Drawing.Size(372, 138);
+            this.startGameBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.startGameBtn.TabIndex = 2;
+            this.startGameBtn.TabStop = false;
+            this.startGameBtn.Click += new System.EventHandler(this.startGameBtnClick);
+            // 
+            // dinoDiplo
+            // 
+            this.dinoDiplo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dinoDiplo.BackColor = System.Drawing.Color.Transparent;
+            this.dinoDiplo.Image = ((System.Drawing.Image)(resources.GetObject("dinoDiplo.Image")));
+            this.dinoDiplo.Location = new System.Drawing.Point(308, 804);
+            this.dinoDiplo.Name = "dinoDiplo";
+            this.dinoDiplo.Size = new System.Drawing.Size(244, 138);
+            this.dinoDiplo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dinoDiplo.TabIndex = 3;
+            this.dinoDiplo.TabStop = false;
+            this.dinoDiplo.Click += new System.EventHandler(this.diploClick);
+            // 
+            // dinoSteggy
+            // 
+            this.dinoSteggy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dinoSteggy.BackColor = System.Drawing.Color.Transparent;
+            this.dinoSteggy.Image = ((System.Drawing.Image)(resources.GetObject("dinoSteggy.Image")));
+            this.dinoSteggy.Location = new System.Drawing.Point(552, 813);
+            this.dinoSteggy.Name = "dinoSteggy";
+            this.dinoSteggy.Size = new System.Drawing.Size(244, 138);
+            this.dinoSteggy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dinoSteggy.TabIndex = 4;
+            this.dinoSteggy.TabStop = false;
+            this.dinoSteggy.Click += new System.EventHandler(this.steggyClick);
+            // 
+            // dinoParalolo
+            // 
+            this.dinoParalolo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dinoParalolo.BackColor = System.Drawing.Color.Transparent;
+            this.dinoParalolo.Image = ((System.Drawing.Image)(resources.GetObject("dinoParalolo.Image")));
+            this.dinoParalolo.Location = new System.Drawing.Point(791, 801);
+            this.dinoParalolo.Name = "dinoParalolo";
+            this.dinoParalolo.Size = new System.Drawing.Size(244, 138);
+            this.dinoParalolo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dinoParalolo.TabIndex = 5;
+            this.dinoParalolo.TabStop = false;
+            this.dinoParalolo.Click += new System.EventHandler(this.paraloloClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1620, 999);
+            this.Controls.Add(this.dinoParalolo);
+            this.Controls.Add(this.dinoSteggy);
+            this.Controls.Add(this.dinoDiplo);
+            this.Controls.Add(this.startGameBtn);
+            this.Controls.Add(this.instBtn);
+            this.Controls.Add(this.titlePic);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cosmo Dino";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.moveDinos);
+            ((System.ComponentModel.ISupportInitialize)(this.titlePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startGameBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dinoDiplo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dinoSteggy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dinoParalolo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox titlePic;
+        private System.Windows.Forms.PictureBox instBtn;
+        private System.Windows.Forms.PictureBox startGameBtn;
+        private System.Windows.Forms.PictureBox dinoDiplo;
+        private System.Windows.Forms.PictureBox dinoSteggy;
+        private System.Windows.Forms.PictureBox dinoParalolo;
+    }
+}
+
---
+++ Cosmo Dino Game/Cosmo Dino Game/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cosmo_Dino_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TODO: Things to be done when form loads and is ready goes here
+            //Centering the title of the game horizontally
+            titlePic.Left = (this.ClientSize.Width - titlePic.Width) / 2;
+            //Centering the instructions button horizontally
+            instBtn.Left = (this.ClientSize.Width - instBtn.Width) / 2;
+            //Centering the choose dino label horizontally
+            startGameBtn.Left = (this.ClientSize.Width - startGameBtn.Width) / 2;
+        }
+
+        private void instBtnClick(object sender, EventArgs e)
+        {
+            //TODO: Show instructions
+            MessageBox.Show("Show instructions");
+        }
+
+        //Check which dino was selested
+        PictureBox dino;
+
+        private void diploClick(object sender, EventArgs e)
+        {
+            //Diplo selected
+            dino = dinoDiplo;
+            //Other dinos made invisible
+            dinoSteggy.Visible = false;
+            dinoParalolo.Visible = false;
+        }
+
+        private void steggyClick(object sender, EventArgs e)
+        {
+            //Steggy selected
+            dino = dinoSteggy;
+            //Other dinos made invisible
+            dinoDiplo.Visible = false;
+            dinoParalolo.Visible = false;
+        }
+
+        private void paraloloClick(object sender, EventArgs e)
+        {
+            //Paralolo clicked
+            dino = dinoParalolo;
+            //Other dinos made invisible
+            dinoSteggy.Visible = false;
+            dinoDiplo.Visible = false;
+        }
+
+        private void startGameBtnClick(object sender, EventArgs e)
+        {
+            //TODO: handle start game button click
+            if (dino != null)
+            {
+                //Center the dino
+                dino.Left = (this.ClientSize.Width - dino.Width) / 2;
+                //Make all elements invisible
+                titlePic.Visible = false;
+                instBtn.Visible = false;
+                startGameBtn.Visible = false;
+            }               
+            else
+                //Null reference to the dino Control, user warned to select a dino
+                MessageBox.Show("Please select a dino first!");
+        }
+
+        //Find out if the dino is facing left
+        private bool lTurn = true;
+
+        //ALL THE MAIN CODE FOR THE GAME GOES HERE
+        private void moveDinos(object sender, KeyEventArgs e)
+        {
+            //Checking for left key press and absence of title image
+            if (e.KeyCode == Keys.Left && titlePic.Visible == false)
+            {
+                //If the dino is not facing left, flip the dino on left key press
+                if (lTurn != true)
+                {
+                    dino.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    lTurn = true;
+                }
+                //Else if the dino is facing left, move left. Limit: client's boundary
+                else
+                {
+                    if (dino.Left > 0)
+                    {
+                        //Sub. 10 pixels from the dino's distance from the left boundary (Moves dino left)
+                        dino.Left -= 10;
+                    }
+                }
+            }
+            //Checking for right key press and absence of title image
+            else if (e.KeyCode == Keys.Right && titlePic.Visible == false)
+            {
+                //If the dino is facing left, it must now face right on right key press
+                if (lTurn == true)
+                {
+                    dino.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    lTurn = false;
+                }
+                //Else if the dino is already facing the right, move right. Limit: client's boundary
+                else
+                { 
+                    if (dino.Left < this.ClientSize.Width - dino.Width)
+                    {
+                        //Add 10 pixels to the dino's distance from the left boundary (Moves dino right)
+                        dino.Left += 10;
+                    }
+                        
+                }
+            }
+
+            //TODO: main code goes here
+
+        }
+
+        ////
+        ////WHSP
+        ////
+    }
+}
